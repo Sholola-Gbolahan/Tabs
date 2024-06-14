e846b4d5fcc2bea14fa05e43073c45d77c8018a4
Gbolahan <shololagbolahan@gmail.com>
2024-06-14 04:50:06 +0100

    fetched Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+import data from "./data";
+
+// const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [jobs, setJobs] = useState([data]);
+  // const [isLoading, setIsLoading] = useState(true);
+
+  // const fetchData = async () => {
+  //   const resp = await fetch(url);
+  //   const newJobs = await resp.json();
+  //   setJobs(newJobs);
+  //   setIsLoading(false);
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  console.log(jobs);
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
